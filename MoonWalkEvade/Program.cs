82a15cde071bf661dde2adbfc9ad28b8071f28fc
DanThePman <danielvelber@yahoo.de>
2016-07-19 23:40:17 +0200

    s

--- MoonWalkEvade/Program.cs
+++ MoonWalkEvade/Program.cs
@@ -1,6 +1,4 @@
-﻿using System.Linq;
-using EloBuddy;
-using EloBuddy.SDK.Events;
+﻿using EloBuddy.SDK.Events;
 using MoonWalkEvade.Evading;
 using MoonWalkEvade.Skillshots;
 using MoonWalkEvade.Utils;
@@ -9,7 +7,7 @@ namespace MoonWalkEvade
 {
     internal static class Program
     {
-        public static bool DeveloperMode = true;
+        public static bool DeveloperMode = false;
 
         private static SpellDetector _spellDetector;
 
